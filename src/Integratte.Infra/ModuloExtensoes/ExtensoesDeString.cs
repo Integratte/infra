e837883ed2d75e598f338123ad6be2cf087745ff
Cid Prata <cid.prata@wundermanthompson.com>
2022-05-24 06:38:55 -0300

    Extensões de String

---
+++ src/Integratte.Infra/ModuloExtensoes/ExtensoesDeString.cs
@@ -0,0 +1,29 @@
+﻿namespace Integratte.Infra.ModuloExtensoes;
+
+public static class ExtensoesDeString
+{
+    public static bool NuloOuVazio(this string? texto)
+    {
+        return string.IsNullOrEmpty(texto);
+
+    }
+
+    public static string SomenteLetrasOuNumeros(this string texto)
+    {
+        return new string(texto.ToArray().Where(x => char.IsLetterOrDigit(x)).ToArray());
+
+    }
+
+    public static string SomenteNumeros(this string texto)
+    {
+        return new string(texto.ToArray().Where(x => char.IsNumber(x)).ToArray());
+
+    }
+
+    public static T ToEnum<T>(this string value)
+    {
+        return (T)Enum.Parse(typeof(T), value, true);
+
+    }
+
+}
