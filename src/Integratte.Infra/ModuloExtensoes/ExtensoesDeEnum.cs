1002cccdc5c0c82a577d6603b54d0f5ff687a695
Cid Prata <cid.prata@wundermanthompson.com>
2022-05-30 07:24:12 -0300

    Extensões de Enum.

---
+++ src/Integratte.Infra/ModuloExtensoes/ExtensoesDeEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace Integratte.Infra.ModuloExtensoes;
+
+public static class ExtensoesDeEnum
+{
+    public static T ConvertToEnum<T>(this Enum valor)
+    {
+        return (T)Enum.Parse(typeof(T), valor.ToString(), true);
+
+    }
+
+    public static (int Id, string Nome) ToTuple(this Enum valor)
+    {
+        return (Convert.ToInt32(valor), valor.ToString());
+
+    }
+
+}
