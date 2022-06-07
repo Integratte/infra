53fd58c56c6083106fe2b6fc7b4b02713d1a5ef6
Cid Prata <cid.prata@wundermanthompson.com>
2022-06-07 16:25:09 -0300

    Normalização do método executar de uma chamada http (síncrono e assíncrono)

--- src/Integratte.Infra/ModuloHttp/ChamadaHttp.cs
+++ src/Integratte.Infra/ModuloHttp/ChamadaHttp.cs
@@ -1,4 +1,6 @@
-﻿using Integratte.Infra.ModuloExcecoesPersonalizadas;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using Integratte.Infra.ModuloExcecoesPersonalizadas;
 using Newtonsoft.Json;
 
 namespace Integratte.Infra.ModuloHttp;
@@ -8,6 +10,18 @@ public abstract class ChamadaHttp
     protected RequisicaoHttp RequisicaoHttp { get; private set; }
     public string? ConteudoQuandoOcorreErro { get; protected set; }
 
+    public void Executar(RequisicaoHttp requisicao)
+    {
+        Task.FromResult(ExecutarAsync(requisicao));
+
+    }
+
+    public T? Executar<T>(RequisicaoHttp requisicao)
+    {
+        return ExecutarAsync<T>(requisicao).Result;
+
+    }
+
     public async Task ExecutarAsync(RequisicaoHttp requisicao)
     {
         RequisicaoHttp = requisicao;
@@ -15,7 +29,8 @@ public abstract class ChamadaHttp
 
 
     }
-    public async Task<T?> Executar<T>(RequisicaoHttp requisicao)
+
+    public async Task<T?> ExecutarAsync<T>(RequisicaoHttp requisicao)
     {
         RequisicaoHttp = requisicao;
         var (sucesso, conteudo) = await ExecutarChamadaAsync();
